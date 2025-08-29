70bbafcc3d993c4f54a3bd0cb2c6c6ed6334ef3c
rodrex25 <gabresh25@gmail.com>
2025-08-28 21:46:42 -0600

    final update

--- Api_Impl/obj/Debug/net9.0/Api_Impl.AssemblyInfo.cs
+++ Api_Impl/obj/Debug/net9.0/Api_Impl.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api_Impl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1fe925e6db134760392ed699aeb6cfcfe059be5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+527ab0033f0d8987881c4dbacb61b4d69656fe4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api_Impl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api_Impl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
